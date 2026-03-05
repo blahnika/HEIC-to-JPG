83ccee6ed223cf3b071852338a16bf398813ab9a
blahnika <blahnika@me.com>
2026-03-04 22:14:51 -0500

    Update Program.cs
    fixed issue with install error

--- src/Launcher/Program.cs
+++ src/Launcher/Program.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 
 var dir = AppContext.BaseDirectory;
 var script = Path.Combine(dir, "heic_converter.py");
-var args = string.Join(" ", Environment.GetCommandLineArgs()[1..]
+var quotedArgs = string.Join(" ", Environment.GetCommandLineArgs()[1..]
                                .Select(a => $"\"{a}\""));
-Process.Start(new ProcessStartInfo("python", $"\"{script}\" {args}")
+Process.Start(new ProcessStartInfo("python", $"\"{script}\" {quotedArgs}")
     { UseShellExecute = false })?.WaitForExit();
